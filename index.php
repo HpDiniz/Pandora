3bcb619c03743b9a001c94b93adc8f4e244015b1
DenisDk9 <denis.lopes3012@gmail.com>
2018-07-03 07:57:57 -0300

    Arquivo css separado e titulo

--- index.php
+++ index.php
@@ -25,146 +25,24 @@
 <DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<title> </title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <title>Pagina inicial</title>
 
 
 
 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style tyle="text/css">
-
-"{
-
-	margin:0;
-	padding:0;
-
-}
-
-body{
-
-	background-color:#000000;
-
-}
-
-#check{
-
-	display: none;
-
-}
-
-#icone{
-
-	transition:all .2s linear;
-	cursor:pointer;
-	padding :15px;
-	position:absolute;
-	z-index:1;
-
-}
-
-.barra{
-
-	background-color: #333;
-	height:100%;
-	width:300px;
-	position: absolute;
-	transition:all .2s linear;
-	left:-300px;
-}
-
-nav{
-	width:100%;
-	position:absolute;
-	top:60px;
-
-}
-
-nav .a{
-
-	text-decoration: none;
-
-}
-
-.link{
-
-	background-color:494950;
-	padding: 20px;
-	font-family:'arial';
-	font-size: 12pt;
-	transition: all .2s linear;
-	color: #f4f4f9;
-	border-bottom: 2px solid #222;
-	opacity:0;
-	margin-top:200px;
-}
-
-.link:hover{
-	background-color: #050542;
-
-}
-
-
-#check:checked ~ .barra {
-
-	transform: translateX(300px);	
-	
-}
-
-#check:checked ~ #icone{
-
-	transform:translateX(300px);
-	transition: all .2s linear;
-
-}
-
-#check:checked ~ .barra nav a .link{
-
-	opacity:1;	
-	margin-top:0;
-	
-}
-.botao1 {
-    background-color: #999999;
-    border: solid;
-    border-color: #CCCCCC;
-	color:white;
-    padding: 15px;
-    text-align: center;
-    text-decoration: none;
-   
-    font-size: 12px;
-	border-radius: 14px;
-}	
-
-.w3-sidebar a {font-family: "Roboto", sans-serif}
-	body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif; color: black;}
-	body {
-		
-	    background-color: #cccccc;
-	}
-	
-	table, th, td {
-    border: 1px solid black;
-}
-th{
-	
-	background-color:#F5F5F5;
-	
-}
-
-</style>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">    
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
+
+    
 <body>
-
-
 
 <div style="width:all;height:270px;border:1px solid #000; background-color:#651296;z-index:150;">
 	<input type="checkbox" id="check">
